37090c45bc1df4faf5dc6b6a16ceefaccb8ac72e
Nick Leeson <105472205+2112529@users.noreply.github.com>
2023-06-03 10:11:40 +0800

    Add files via upload

---
+++ robot_game.cpp
@@ -0,0 +1,56 @@
+﻿// robot_game.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+const int MAXN = 500010;
+int n, d, k;
+int g=-1;
+
+struct pair {
+    int x;
+    int s;
+}pairs[MAXN];
+long long score;
+
+
+int main()
+{
+    cin >> n >> d >> k;
+    for (int i = 1; i <= n; i++)
+        cin >> pairs[i].x >> pairs[i].s;
+
+    //1.选择贪心算法进行求解，实现局部最优
+    //每一次都选择范围内最优的进行求解
+    while (score < k)
+    {
+        //如何确定循环一定能够终止？
+        g++;
+        for (int i = max(d - g, 1); i <= d + g; i++)
+        {
+            //检索出pairs中最优的选择
+        }
+    }
+    cout << g << endl;
+    return 0;
+    //2.采用dfs进行深度优先搜索尝试求解
+     
+     
+    //3.选择动态规划进行求解
+
+    
+    
+    //return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
